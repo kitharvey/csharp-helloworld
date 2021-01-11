a928a4e31aa66a733b9840d4a063b0df028c4636
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-01-11 09:20:42 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace java_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
